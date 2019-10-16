7862942318f4fa2743e2eab9b065c7604dc8a9f6
Niels Berglund <niels.it.berglund@gmail.com>
2019-10-16 05:51:40 +0200

    Added .gitignore and some code

---
+++ array/test1.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class test1 {
+  
+  public static void main (string[] args) {
+    int[] col1 = new int[5];
+    boolean[] nullCol1 = new boolean[5];
+
+    
+    
+    
+
+  }
+
+}
